e5092d11b79ab579f228c47c21ccd51be6c3ea20
darthpetter <gavilanezseg@gmail.com>
2022-02-15 20:26:39 -0500

    Scential code

--- app/src/main/java/com/example/a3enraya/MainActivity.kt
+++ app/src/main/java/com/example/a3enraya/MainActivity.kt
@@ -20,5 +20,31 @@ class MainActivity : AppCompatActivity() {
     fun pressButton(view: View){
         val button=view as Button
         button.setText("$player")
+        player=changePlayer(player)
+        button.setEnabled(false)
+        println(defineLoser(player))
+    }
+
+    private fun changePlayer(player:Char):Char{
+        return when(player){
+            'X' -> 'O'
+            'O' -> 'X'
+            else -> 'p'
+        }
+    }
+
+    private fun defineLoser(player:Char):String{
+        if(binding.btn1.text.toString()==player.toString() && binding.btn2.getText().toString()==player.toString() && binding.btn3.getText().toString()==player.toString()){
+            return "Ganó el jugador $player"
+        }else if(binding.btn4.text.toString()==player.toString() && binding.btn5.getText().toString()==player.toString() && binding.btn6.getText().toString()==player.toString()){
+            return "Ganó el jugador $player"
+        }else if(binding.btn7.text.toString()==player.toString() && binding.btn8.getText().toString()==player.toString() && binding.btn9.getText().toString()==player.toString()){
+            return "Ganó el jugador $player"
+        }else if(binding.btn1.text.toString()==player.toString() && binding.btn5.getText().toString()==player.toString() && binding.btn9.getText().toString()==player.toString()){
+            return "Ganó el jugador $player"
+        }else if(binding.btn3.text.toString()==player.toString() && binding.btn2.getText().toString()==player.toString() && binding.btn7.getText().toString()==player.toString()){
+            return "Ganó el jugador $player"
+        }
+        return "No se define ganador aún."
     }
 }
