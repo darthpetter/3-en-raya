b000526491e99a8a91a511a7350c8d8f1756d9c9
darthpetter <gavilanezseg@gmail.com>
2022-02-15 21:02:52 -0500

    Basic functions developed

--- app/src/main/java/com/example/a3enraya/MainActivity.kt
+++ app/src/main/java/com/example/a3enraya/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import com.example.a3enraya.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,9 +21,9 @@ class MainActivity : AppCompatActivity() {
     fun pressButton(view: View){
         val button=view as Button
         button.setText("$player")
-        player=changePlayer(player)
         button.setEnabled(false)
-        println(defineLoser(player))
+        if(missingWinner(player)) {winnerFounded()}
+        player=changePlayer(player)
     }
 
     private fun changePlayer(player:Char):Char{
@@ -33,18 +34,42 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun defineLoser(player:Char):String{
-        if(binding.btn1.text.toString()==player.toString() && binding.btn2.getText().toString()==player.toString() && binding.btn3.getText().toString()==player.toString()){
-            return "Ganó el jugador $player"
-        }else if(binding.btn4.text.toString()==player.toString() && binding.btn5.getText().toString()==player.toString() && binding.btn6.getText().toString()==player.toString()){
-            return "Ganó el jugador $player"
-        }else if(binding.btn7.text.toString()==player.toString() && binding.btn8.getText().toString()==player.toString() && binding.btn9.getText().toString()==player.toString()){
-            return "Ganó el jugador $player"
-        }else if(binding.btn1.text.toString()==player.toString() && binding.btn5.getText().toString()==player.toString() && binding.btn9.getText().toString()==player.toString()){
-            return "Ganó el jugador $player"
-        }else if(binding.btn3.text.toString()==player.toString() && binding.btn2.getText().toString()==player.toString() && binding.btn7.getText().toString()==player.toString()){
-            return "Ganó el jugador $player"
+    private fun missingWinner(player:Char):Boolean{
+        var playerStr=player.toString()
+        if(binding.btn1.getText().toString()==playerStr && binding.btn2.getText().toString()==playerStr && binding.btn3.getText().toString()==playerStr){
+            return true
+        }else if(binding.btn4.getText().toString()==playerStr && binding.btn5.getText().toString()==playerStr && binding.btn6.getText().toString()==playerStr){
+            return true
+        }else if(binding.btn7.getText().toString()==playerStr && binding.btn8.getText().toString()==playerStr && binding.btn9.getText().toString()==playerStr){
+            return true
+        }else if(binding.btn1.getText().toString()==playerStr && binding.btn5.getText().toString()==playerStr && binding.btn9.getText().toString()==playerStr){
+            return true
+        }else if(binding.btn3.getText().toString()==playerStr && binding.btn2.getText().toString()==playerStr && binding.btn7.getText().toString()==playerStr) {
+            return true
+        }else if(binding.btn1.getText().toString()==playerStr && binding.btn4.getText().toString()==playerStr && binding.btn7.getText().toString()==playerStr){
+            return true
+        }else if(binding.btn2.getText().toString()==playerStr && binding.btn5.getText().toString()==playerStr && binding.btn8.getText().toString()==playerStr){
+            return true
         }
-        return "No se define ganador aún."
+        else if(binding.btn3.getText().toString()==playerStr && binding.btn6.getText().toString()==playerStr && binding.btn9.getText().toString()==playerStr){
+            return true
+        }
+        return false
+    }
+    private fun winnerFounded(){
+        blockButtons()
+        Toast.makeText(this,"Winner: $player.\uD83C\uDFC6",Toast.LENGTH_SHORT).show()
+    }
+
+    private fun blockButtons(){
+        binding.btn1.setEnabled(false)
+        binding.btn2.setEnabled(false)
+        binding.btn3.setEnabled(false)
+        binding.btn4.setEnabled(false)
+        binding.btn5.setEnabled(false)
+        binding.btn6.setEnabled(false)
+        binding.btn7.setEnabled(false)
+        binding.btn8.setEnabled(false)
+        binding.btn9.setEnabled(false)
     }
 }
